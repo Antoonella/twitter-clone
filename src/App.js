294f889c366cf38ef8a916b5bbeb69a1ffde1116
antonella <antonella.makahe@gmail.com>
2023-01-04 03:20:36 +1100

    adding feed section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Feed from './Feed';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Feed />
     </div>
   );
 }
